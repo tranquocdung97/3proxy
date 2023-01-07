4398813f5d0c33a6e27e2b230bc6f9127414f75a
Trần Quốc Dũng <tranquocdung10081997@gmail.com>
2023-01-08 00:01:02 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -144,7 +144,7 @@ gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 cat <<EOF > /etc/rc.local
 systemctl start NetworkManager.service
 ifup ${NWINAME}
-bash ${WORKDIR}/boot_iptableseee.sh
+bash ${WORKDIR}/boot_iptables.sh
 bash ${WORKDIR}/boot_ifconfig.sh
 ulimit -n 65535
 /usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg &
